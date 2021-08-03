c37a10dff942fc89947d5fcc7ce63496ec6ab292
joelmanibi <bi.mani@uvci.edu.ci>
2021-08-03 12:52:27 +0000

    Update settings.py

--- VeilleTech/settings.py
+++ VeilleTech/settings.py
@@ -135,6 +135,10 @@ if os.environ.get('ENV') == 'PRODUCTION':
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+    )
+
     STATICFILES_STORAGE ='whitenoise.storage.CompresseManifestStaticFilesStorage'
 
     db_from_env = dj_database_url.config(conn_max_age=500)
