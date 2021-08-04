10c24ee23686ef44ca44a67fa3d2c15d7b5bd17a
joelmanibi <bi.mani@uvci.edu.ci>
2021-08-04 14:12:01 +0000

    Update settings.py

--- VeilleTech/settings.py
+++ VeilleTech/settings.py
@@ -134,7 +134,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
     
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-    STATIC_URL = '/static/'
+    #STATIC_URL = '/static/'
 
     STATICFILES_DIRS = (
         os.path.join(PROJECT_ROOT, 'static'),
