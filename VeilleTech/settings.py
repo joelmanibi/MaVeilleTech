1db58863f22e42eb0f231e86c5a5a88919db57ba
joelmanibi <bi.mani@uvci.edu.ci>
2021-08-12 10:55:23 +0000

    Update settings.py

--- VeilleTech/settings.py
+++ VeilleTech/settings.py
@@ -15,7 +15,7 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates/')
 
 
@@ -130,23 +130,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-if os.environ.get('ENV') == 'PRODUCTION':
     
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-    #STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Extra places for collectstatic to find static files.
 
-    STATICFILES_STORAGE ='whitenoise.storage.CompresseManifestStaticFilesStorage'
+#STATICFILES_STORAGE ='whitenoise.storage.CompresseManifestStaticFilesStorage'
 
-    db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=500)
 
-    DATABASES['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
